ac7b450b1bec1de402a34a21db6a31456f05a3e4
Zainab Anam <zainab.anam@mail.utornoto.ca>
2025-11-10 10:14:35 -0500

    Added some instance variables

--- src/main/java/game/entity/User.java
+++ src/main/java/game/entity/User.java
@@ -1,5 +1,20 @@
 package game.entity;
 
+import java.util.ArrayList;
+
 public class User {
-    
+    /*
+    PetInventory: List<Pet>
+    itemsList: List<Item>
+    + coinCount: int
+    clickBonus (Click multiplier; how many coins you get per click):int
+    clickBonusTime:int
+    petSlot:int
+    */
+    ArrayList<Pet> PetInventory = new ArrayList<Pet>();
+    ArrayList<Item> ItemInventory = new ArrayList<Item>();
+    int coinCount = 0;
+    int clickBonus = 0;
+    int slotsUnlocked = 0;
+
 }
