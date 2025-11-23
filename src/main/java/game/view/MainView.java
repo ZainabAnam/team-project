b2841bdd90be9f524ba562e2c322fa9aa7bda04d
Zainab Anam <zainab.anam@mail.utornoto.ca>
2025-11-23 16:00:49 -0500

    Added the Menu Buttons: Shop, Collections, Save

--- src/main/java/game/view/MainView.java
+++ src/main/java/game/view/MainView.java
@@ -17,28 +17,21 @@ public class MainView extends JPanel{
     private final ImageIcon clickerImage = new ImageIcon(getClass().getResource("/images/Clicker.png"));
     private final ImageIcon clickerClickedImage = new ImageIcon(getClass().getResource("/images/ClickerClicked.png"));
 
-    // Buttons
-    private final JButton clicker;
-    private final Slot slot1;
-    private final Slot slot2;
-    private final Slot slot3;
-    private final Slot slot4;
-
     public MainView() {
 
         setPreferredSize(new Dimension(720, 540));
         setLayout(null);
 
-        // Instantiating Slots
-        this.slot1 = new Slot(true);  // unlocked at start
-        this.slot2 = new Slot(false);
-        this.slot3 = new Slot(false);
-        this.slot4 = new Slot(false);
-
-        clicker = getClicker();
-
+        // Setting up/adding the main clicker.
+        JButton clicker = getClicker();
         clicker.setBounds(260, 50, 200, 200); // x, y, width, height
         add(clicker);
+
+        // Instantiating Slots.
+        Slot slot1 = new Slot(true);  // unlocked at start
+        Slot slot2 = new Slot(false);
+        Slot slot3 = new Slot(false);
+        Slot slot4 = new Slot(false);
 
         final JPanel slotsPanel = new JPanel();
         slotsPanel.setOpaque(false);
@@ -50,6 +43,26 @@ public class MainView extends JPanel{
 
         slotsPanel.setBounds(110, 280, 520, 150);
         add(slotsPanel);
+
+        // Menu Buttons
+        JButton shop = new JButton("Shop");
+        shop = getMenuButton(shop);
+        JButton collections = new JButton("Collections");
+        collections = getMenuButton(collections);
+        JButton save =  new JButton("Save");
+        save = getMenuButton(save);
+
+        final JPanel menuButtons = new JPanel();
+        menuButtons.setOpaque(false);
+        menuButtons.add(shop);
+        menuButtons.add(collections);
+        menuButtons.add(save);
+        menuButtons.setLayout(new GridLayout(1, 3, 80, 0));
+
+        menuButtons.setBounds(55, 480, 610, 50);
+        add(menuButtons);
+
+
     }
 
     // Making a clicker JButton with custom graphic.
@@ -63,6 +76,15 @@ public class MainView extends JPanel{
         clicker.setPressedIcon(clickerClickedImage);
         return clicker;
     }
+
+    // Customizing other menu buttons
+    private JButton getMenuButton(JButton menuButton) {
+        menuButton.setSize(150, 50);
+        menuButton.setBackground(Color.orange);
+        menuButton.setBorderPainted(false);
+        return menuButton;
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
