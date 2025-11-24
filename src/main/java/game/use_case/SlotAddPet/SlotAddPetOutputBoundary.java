fe4a66d0354b4e568df365244627ec63ff627f74
Zainab Anam <zainab.anam@mail.utornoto.ca>
2025-11-23 19:28:58 -0500

    Implementing the Add Pet Usecase

---
+++ src/main/java/game/use_case/SlotAddPet/SlotAddPetOutputBoundary.java
@@ -0,0 +1,5 @@
+package game.use_case.SlotAddPet;
+
+public class SlotAddPetOutputBoundary {
+    public Object prepareFailView;
+}
---
+++ src/main/java/game/use_case/SlotAddPet/SlotAddPetOutputData.java
@@ -0,0 +1,12 @@
+package game.use_case.SlotAddPet;
+
+import game.entity.*;
+
+/**
+ * The input data for the slot clicked use case.
+ */
+public class SlotAddPetOutputData {
+
+
+
+}
