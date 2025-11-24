477a76fd9242cda577f1da22169869074c66c6a5
peterluo <realpeter.luo@mail.utoronto.ca>
2025-11-24 12:54:23 -0500

    RenamePet Test finished

--- src/test/java/game/use_case/SellPet/SellPetInteractorTest.java
+++ src/test/java/game/use_case/SellPet/SellPetInteractorTest.java
@@ -1,3 +1,9 @@
 package game.use_case.SellPet;
 
+import game.entity.Pet;
+import game.entity.User;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import game.use_case.PetCard.SellPet.*;
+
 class SellPetInteractorTest {}
