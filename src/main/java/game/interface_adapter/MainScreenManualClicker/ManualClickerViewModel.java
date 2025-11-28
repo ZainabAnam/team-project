6b8c34df872a54b44ccc8eb9fed9e06dacc065fd
tiffany_lin <tiffanyg.lin@mail.utoronto.ca>
2025-11-28 13:38:57 -0500

    initial commit to help merge from main

--- src/main/java/game/interface_adapter/MainScreenManualClicker/ManualClickerViewModel.java
+++ src/main/java/game/interface_adapter/MainScreenManualClicker/ManualClickerViewModel.java
@@ -1,8 +1,10 @@
-//package game.interface_adapter.MainScreenManualClicker;
-//
-//import game.interface_adapter.ViewModel;
-//import game.interface_adapter.MainScreenManualClicker.ManualClickerState;
-//
-//public class ManualClickerViewModel extends ViewModel<ManualClickerState> {
-//    public ManualClickerViewModel() {}
-//}
+package game.interface_adapter.MainScreenManualClicker;
+
+import game.interface_adapter.ViewModel;
+import game.interface_adapter.MainScreenManualClicker.ManualClickerState;
+
+public class ManualClickerViewModel extends ViewModel<ManualClickerState> {
+    public ManualClickerViewModel() {
+        super("ManualClicker");
+    }
+}
