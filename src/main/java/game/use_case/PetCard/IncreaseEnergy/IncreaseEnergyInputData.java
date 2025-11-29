ff547bd555ab43bf2910cc7edc4f322ac6983149
tiffany_lin <tiffanyg.lin@mail.utoronto.ca>
2025-11-29 11:11:21 -0500

    committing to check out to main, still in progress

--- src/main/java/game/use_case/PetCard/IncreaseEnergy/IncreaseEnergyInputData.java
+++ src/main/java/game/use_case/PetCard/IncreaseEnergy/IncreaseEnergyInputData.java
@@ -1,12 +1,18 @@
 package game.use_case.PetCard.IncreaseEnergy;
 
 public class IncreaseEnergyInputData {
+    private final String userID;
     private final String petName;
     private final int energyIncrease;
 
-    public IncreaseEnergyInputData(String petName,  int energyBoost) {
+    public IncreaseEnergyInputData(String userID, String petName,  int energyBoost) {
+        this.userID = userID;
         this.petName = petName;
         this.energyIncrease = energyBoost;
+    }
+
+    public String getUserID() {
+        return userID;
     }
 
     public String getPetName() {
